997c2492e81d51468f11d483370917d2ec0a9c58
abhishek2432001 <abhishekpandey2432001@gmail.com>
2024-04-06 20:22:24 +0530

    Learning react this weekend

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+
+function App() {
+  const username = "Abhishek";
+
+  return (
+    <>
+    <Chai/>
+    <h4>CHai aur {username}</h4>
+    </>
+  )
+}
+
+export default App
---
+++ 01viteReact/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+  
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+    <App />
+    <h1>chai in vite</h1>
+    </>
+    
+)
